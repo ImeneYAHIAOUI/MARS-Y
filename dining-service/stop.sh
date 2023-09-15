726d205eba67ea26906f1467fec7faa9913e42fa
sourour9 <sourour.gazzeh@outlook.fr>
2023-09-15 22:57:58 +0200

    add weather service

--- dining-service/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "stopping dining-service"
-docker-compose --env-file ./.env.docker \
-               --file docker-compose-dining-alone.yml down
--- menu-service/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "stopping menu-service"
-docker-compose --env-file ./.env.docker \
-               --file docker-compose-menu.yml down
