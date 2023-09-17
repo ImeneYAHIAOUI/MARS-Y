dd87634116d1db02db4c57eb0e06ba4e8ddf0429
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2023-09-17 20:18:01 +0200

    change .sh files #1

--- build-all.sh
+++ build-all.sh
@@ -10,7 +10,8 @@ function compile_dir()  # $1 is the dir to get it
 echo "** Compiling all"
 
 compile_dir "marsy-weather"
-
+compile_dir "marsy-mission"
+compile_dir "marsy-rocket"
 
 echo "** Done all"
 
--- start-all.sh
+++ start-all.sh
@@ -4,6 +4,8 @@ source ./framework.sh
 
 echo "starting all"
 docker-compose --env-file ./.env.docker \
-               --file marsy-weather/docker-compose.yml  up -d
+               --file marsy-weather/docker-compose.yml \
+               --file marsy-rocket/docker-compose-marsy-rocket.yml \
+               --file marsy-mission/docker-compose-marsy-mission.yml up -d
 
 echo "all services started behind gateway"
