eb5fd7bd40ce783545305c3ecf898deaaeaf409d
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2023-09-17 23:19:26 +0200

    add e2e test

--- marsy-mission/test/app.e2e-spec.ts
+++ marsy-mission/test/app.e2e-spec.ts
@@ -1,24 +1,49 @@
+import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
-describe('AppController (e2e)', () => {
+import appConfig from '../src/shared/config/app.config';
+import { GoPollController } from '../src/go-poll/controllers/go-poll.controller';
+import { GoPollService } from '../src/go-poll/services/go-poll.service';
+
+
+describe('GoPollController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  const mockGoPollService = {
+    goOrNoGoPoll: (rocketId: string) => true, // Mock the goOrNoGoPoll method as needed
+  };
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [appConfig],
+        }),
+        HttpModule, // Add HttpModule
+      ],
+      controllers: [GoPollController], // Add your GoPollController
+      providers: [GoPollService], // Add your GoPollService
+    })
+      .overrideProvider(GoPollService)
+      .useValue(mockGoPollService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/go/rocket/:rocketId (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/go/rocket/rocket1') // Replace 'rocket1' with the desired rocket ID
       .expect(200)
-      .expect('Hello World!');
+      .expect({ go: true }); // Adjust the expected response as needed
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
