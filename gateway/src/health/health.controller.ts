5d8b9f1cd133c4f8d061138ed62f460abe22bf69
sourour9 <sourour.gazzeh@outlook.fr>
2023-09-18 13:27:02 +0200

    add logs in marsy weather

--- gateway/src/health/health.controller.ts
+++ gateway/src/health/health.controller.ts
@@ -11,8 +11,6 @@ import { DependenciesConfig } from '../shared/config/interfaces/dependencies-con
 @Controller('health')
 export class HealthController {
   private _marsyRocketServiceHealthCheckUrl: string;
-  private _marsyWeatherServiceHealthCheckUrl: string;
-  private _marsyMissionServiceHealthCheckUrl: string;
 
   constructor(
     private configService: ConfigService,
@@ -22,8 +20,6 @@ export class HealthController {
     const dependenciesConfig =
       this.configService.get<DependenciesConfig>('dependencies');
     this._marsyRocketServiceHealthCheckUrl = `http://${dependenciesConfig.marsy_rocket_service_url_with_port}/health`;
-    //this._marsyWeatherServiceHealthCheckUrl = `http://${dependenciesConfig.marsy_weather_service_url_with_port}/health`;
-    this._marsyMissionServiceHealthCheckUrl = `http://${dependenciesConfig.marsy_mission_service_url_with_port}/health`;
   }
 
   async checkIsHealthy(name, url) {
--- marsy-weather/start.sh
+++ marsy-weather/start.sh
@@ -5,5 +5,4 @@ echo "starting marsy-rocket-service"
 docker-compose --env-file ./.env.docker \
                --file docker-compose-marsy-weather.yml up -d
 
-wait_on_health http://localhost:3002 ${PWD##*/}
 
