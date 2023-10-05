f210652c4e673b645a8be4ac47d46ff43fc3e312
ImeneYAHIAOUI <90778346+ImeneYAHIAOUI@users.noreply.github.com>
2023-10-05 21:54:16 +0200

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -18,7 +18,7 @@ compile_dir "marsy-mock"
 compile_dir "marsy-payload"
 compile_dir "marsy-boostercontrol"
 compile_dir "marsy-guidance"
-compile_dir "marsy-webcaster
+compile_dir "marsy-webcaster"
 
 echo "** Done all"
 
