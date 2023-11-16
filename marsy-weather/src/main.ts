04df8b6db923677a973486e41894356532addbcd
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2023-11-16 21:18:12 +0100

    add swagger in weather service

--- marsy-weather/src/main.ts
+++ marsy-weather/src/main.ts
@@ -16,22 +16,16 @@ async function bootstrap() {
 
   // Add validation pipi for all endpoints
   app.useGlobalPipes(new ValidationPipe());
-
-  // Swagger UI Definition
-  const swaggeruiConfig = configService.get<SwaggerUIConfig>('swaggerui');
-  const config = new DocumentBuilder()
-    .setTitle(swaggeruiConfig.title)
-    .setDescription(swaggeruiConfig.description)
-    .setVersion(configService.get('npm_package_version'))
-    .addServer('/', 'Without gateway')
-    .addServer('/rocket', 'Through gateway')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(swaggeruiConfig.path, app, document);
-
   // Starts listening for shutdown hooks
   app.enableShutdownHooks();
-
+  // Swagger UI Definition
+  const options = new DocumentBuilder()
+      .setTitle('Weather Service')
+      .setDescription('This service provides weather status.')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/doc/weather', app, document);
   // Run the app
   const appPort = configService.get('app.port');
   console.log(appPort)
