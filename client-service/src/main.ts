4a4a60e6b0a786cdad2ce5a2688262be790c4aac
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2023-11-16 21:49:45 +0100

    add swagger in client  service

--- client-service/src/main.ts
+++ client-service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -13,7 +14,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableShutdownHooks();
-
+ // Swagger UI Definition
+  const options = new DocumentBuilder()
+      .setTitle('Broadcast Service')
+      .setDescription('The client service, acting as a mission client, initiates satellite launches and notifies the broadcast service of this event.')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/doc/client', app, document);
   const appPort = configService.get('app.port');
   console.log(appPort);
   await app.listen(appPort);
