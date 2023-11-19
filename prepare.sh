f6da25d4f70cba3c8fddcc0bd08683fbffd1bcea
badr-ach <alachkarbadr@gmail.com>
2023-11-19 19:32:11 +0100

    update : scripts

--- prepare.sh
+++ prepare.sh
@@ -25,3 +25,53 @@ compile_dir "pilot-service"
 
 echo "** Done all"
 
+services=(
+    "marsy-weather:marsy-weather/docker-compose-marsy-weather.yml"
+    "marsy-launchpad:marsy-launchpad/docker-compose-marsy-launchpad.yml"
+    "marsy-mission:marsy-mission/docker-compose-marsy-mission-alone.yml"
+    "marsy-telemetry:marsy-telemetry/docker-compose-marsy-telemetry.yml"
+    "marsy-boostercontrol:marsy-boostercontrol/docker-compose-marsy-booster.yml"
+    "marsy-payload:marsy-payload/docker-compose-marsy-payload.yml"
+    "marsy-guidance:marsy-guidance/docker-compose-marsy-guidance.yml"
+    "marsy-mock:marsy-mock/docker-compose-marsy-mock.yml"
+    "marsy-payload-hardware:marsy-payload-hardware/docker-compose-marsy-payload-hardware.yml"
+    "marsy-webcaster:marsy-webcaster/docker-compose-marsy-webcaster.yml"
+    "client-service:client-service/docker-compose-client-service.yml"
+    "broadcast-service:broadcast-service/docker-compose-broadcast-service.yml"
+    "pilot-service:pilot-service/docker-compose-pilot-service.yml"
+
+)
+container_ids=()
+
+echo "Starting service kafka-service..."
+docker compose --env-file ./.env.docker -f kafka-service/docker-compose-kafka-service.yml up -d
+
+echo "Waiting for the broker to be setup..."
+
+sleep 5
+
+echo "Creating topics..."
+
+./kafka-service/init-topic.sh
+
+start_service() {
+    local service_name=$1
+    local compose_file=$2
+    echo "Starting service $service_name..."
+    docker compose --env-file ./.env.docker -f $compose_file up  -d
+}
+
+# Loop to start all services
+for service in "${services[@]}"; do
+    IFS=':' read -ra service_info <<< "$service"
+    service_name=${service_info[0]}
+    compose_file=${service_info[1]}
+
+    start_service "$service_name" "$compose_file"
+    sleep 1
+done
+
+sleep 2
+
+echo "Done starting services."
+
